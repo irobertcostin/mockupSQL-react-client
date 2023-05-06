1a2a0c2b400c435e740d67b59420e9c92970e39f
irobertcostin <irobertcostin@gmail.com>
2023-05-07 01:58:37 +0300

    task completed

--- src/components/EditCar.jsx
+++ src/components/EditCar.jsx
@@ -2,7 +2,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import Header from "./Header";
 import { Context } from '../context/Context';
 import React, { useState, useEffect, useContext } from 'react';
-
+import { DatePicker, Button, Form, Input, InputNumber, message } from 'antd';
+import Data from "../services/Api";
 
 export default function EditCar() {
 
@@ -14,15 +15,83 @@ export default function EditCar() {
 
 
 
-    let retrieveCar = () => {
+    let [brand, setMaker] = useState('');
+    let [model, setModel] = useState('');
+    let [year, setYear] = useState('');
+    let [price, setPrice] = useState('');
+    let [mileage, setMileage] = useState('');
+
+
+    let onChangeMaker = (element) => {
+        let obj = element.target.value;
+        // console.log(obj);
+        setMaker(obj);
+    }
+
+    let onChangeModel = (element) => {
+        let obj = element.target.value;
+        // console.log(obj);
+        setModel(obj);
+    }
+
+    let onChangePrice = (element) => {
+        setPrice(element)
+        // console.log(element);
+    };
+
+    let onChangeMileage = (element) => {
+        // console.log(element)
+        setMileage(element)
+    };
+
+    let onChangeYear = (element) => {
+        setYear(element.$y + "-" + (element.$M + 1) + "-" + element.$D)
+    };
+
+
+
+    let api = new Data();
+
+
+    let newCar = async () => {
+
+        let newC = {
+            id: +id,
+            maker: brand,
+            model: model,
+            year: year,
+            price: price,
+            mileage: mileage
+        }
+
+        await api.editCar(newC, id)
+
+        let x = data.filter(e => e.id != id);
+
+        x.push(newC);
+
+        console.log(x);
+        setData(x);
+    }
+
+
+
+
+
+
+
+
+
+
+    let retrieveCar = (id) => {
         let x = data.filter(e => e.id == id)
         setEditCar(x[0]);
     }
 
 
     useEffect(() => {
-        retrieveCar()
-    }, [])
+        retrieveCar(id)
+    }, [data])
 
 
     return (
@@ -40,7 +109,74 @@ export default function EditCar() {
                         <p>Car Reg: {editCar.year}</p>
                         <p>Car Mileage: {editCar.mileage}</p>
                         <p>Car Price: ${editCar.price}</p>
+                    </div>
 
+                    <div>
+                        <Form className=" add-form flex flex-col  px-12 py-12  w-auto rounded-lg"
+                            labelCol={{
+                                span: 0,
+                            }}
+                            wrapperCol={{
+                                span: 50,
+
+                            }}
+                            layout="horizontal"
+                            initialValues={{
+                                // size: componentSize,
+                            }}
+                            // onValuesChange={onFormLayoutChange}
+                            // size={componentSize}
+                            label=""
+
+                        >
+
+
+
+                            <Form.Item className=" flex ">
+                                <Input className="border-1 border-gray-300 rounded-lg " onChange={onChangeMaker} style={{ width: '200px' }} placeholder="Maker" />
+                            </Form.Item>
+
+                            <Form.Item className=" flex ">
+                                <Input className="border-1 border-gray-300 rounded-lg " onChange={onChangeModel} style={{ width: '200px' }} placeholder="Model" />
+                            </Form.Item>
+
+
+                            <div className=" flex items-center mb-6">
+                                <label className="mr-12 hidden">Mileage</label>
+                                <InputNumber prefix="km" onChange={onChangeMileage} style={{ width: '200px' }} />
+                            </div>
+
+
+
+                            <div className=" flex items-center mb-6">
+                                <label className="mr-16 hidden">Price</label>
+                                <InputNumber
+                                    defaultValue={1000}
+                                    formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                    parser={(value) => value.replace(/\$\s?|(,*)/g, '')}
+                                    onChange={onChangePrice}
+                                    style={{ width: '200px' }}
+                                />
+                            </div>
+
+                            <div className=" flex items-center mb-6">
+                                <label className="mr-12 hidden">Registration</label>
+                                <DatePicker style={{ width: '200px' }} onChange={onChangeYear} picker="day" placeholder="Registration date" bordered={true} />
+                            </div>
+
+
+
+                            <div className="flex md:w-[00px]  flex-row gap-4 items-center justify-center mt-2 mr-4">
+                                <Button type="primary" onClick={newCar} className="bg-blue-600 text-shadow-glow hover:scale-110">Submit</Button>
+                                <Button type="primary" danger className="bg-blue-600 text-shadow-glow hover:scale-110">Delete</Button>
+                                <Button type="primary" className=" text-shadow-glow hover:scale-110" danger>Cancel</Button>
+                            </div>
+
+
+
+
+
+                        </Form>
                     </div>
                 </div>
             }
--- src/services/Api.js
+++ src/services/Api.js
@@ -30,8 +30,35 @@ export default class Data {
                 let resp = await data.json();
                 return resp;
             } else {
+                let resp = await data.json();
 
+                message.error(resp.error.message, [3], console.log(""))
             }
+
+        } catch (error) {
+            message.error(error, [3], console.log(error))
+        }
+    }
+
+
+
+    async editCar(car, id) {
+        try {
+
+            let data = await this.api(`/edit/id=${id}`, "PUT", car);
+
+            if (data.status === 202) {
+                let resp = await data.json();
+
+                message.success(resp, [3], console.log(""))
+                return resp;
+            } else {
+                let resp = await data.json();
+
+                message.error(resp.error.message, [3], console.log(""))
+            }
+
+
 
         } catch (error) {
             message.error(error, [3], console.log(error))
